67c3aa13cf845b9bdce38ab164aec2b39a7c7bb9
DO GIA DAT <ddogiadat@gmailcom>
2020-03-11 16:08:43 +0700

    Add: level3 feature

---
+++ backend/level3/main.rb
@@ -0,0 +1,26 @@
+require "../setup"
+Dir["./models/*.rb"].each {|file| require file }
+
+
+data_input = DataTranslator.import_from "data/input.json"
+
+Car.all_record = data_input["cars"]
+Rental.all_record = data_input["rentals"]
+
+rental_prices = []
+Rental.all_record.each do |rental|
+  rental = Rental.find(rental["id"])
+  rental_prices.push ({
+    id: rental.id,
+    price: rental.price,
+    commission: {
+      insurance_fee: rental.insurance_fee,
+      assistance_fee: rental.assistance_fee,
+      drivy_fee: rental.drivy_fee
+    }
+  })
+end
+
+data_output = { rentals: rental_prices }
+
+DataTranslator.export_to"test/expected_output.json", data_output
